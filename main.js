f674d45acbab52257c087c324f1abe3dd3c67551
johnnybro360 <abest82@gmail.com>
2022-11-19 22:56:00 +0800

    6th

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // client = new Paho.MQTT.Client("mqtt.eclipseprojects.io", Number(443), "Display");
-client = new Paho.MQTT.Client("mqtt.eclipseprojects.io", Number(80), "/mqtt", "Display");
+client = new Paho.MQTT.Client("wss://mqtt.eclipseprojects.io", 443);
 
 client.onConnectionLost = (responseObject) => console.log("Connection Lost: "+responseObject.errorMessage);
 
